d7d6d8b28a9f781763ed55ee830e7697a862d8eb
JoseCaarlos <josecarlos10967@gmail.com>
2019-06-07 12:46:08 -0300

    Validação customizada: Email não repetido na atualização de cliente

--- cursomc/src/main/java/com/josecarlos/cursomc/dto/ClienteNewDTO.java
+++ cursomc/src/main/java/com/josecarlos/cursomc/dto/ClienteNewDTO.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.josecarlos.cursomc.services.validation.ClienteInsert;
+import com.josecarlos.cursomc.services.validation.ClienteUpdate;
 
 
 @ClienteInsert
